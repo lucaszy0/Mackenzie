6b456746464dfc7a04b4e47dcff75e4418e03ea6
lucas gabriel <gabriel.cardosso13@gmail.com>
2025-05-09 20:26:09 -0300

    Initial commit

---
+++ Python/aula001/python.py
@@ -0,0 +1,23 @@
+
+
+idade = 10
+
+cidade = 'sao paulo'
+iniciante = True
+nota = 9.9
+print(type(idade))
+
+nome = input("Digite seu nome: ")
+idade = int(input("digite sua idade: "))
+idade = float (input("digite seu peso: "))
+print("seja bem vindo ao curso python",nome)
+print("seja bem vindo ao curso python",idade)
+
+n1 = 5
+n2 = 3
+print ("a soma é ", n1 + n2)
+#imc calculo
+peso = float(input("digite o peso:"))
+altura = float (input("digite a altura: "))
+imc = peso / (altura ** 2)
+print( "seu imc é %.2f " %(imc))
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mackenzie
+Cursos Mackenzie 
