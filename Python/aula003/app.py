433b4aa16f4c7c221d7636244ac9489d15ee3eac
lucas gabriel <gabriel.cardosso13@gmail.com>
2025-05-10 23:20:17 -0300

    4.0

---
+++ Python/aula003/app.py
@@ -0,0 +1,12 @@
+#oque devo fazer quando acordar?
+devo_levantar = False
+esta_muito_frio = True
+esta_muito_quente = True
+if devo_levantar == True:
+    print("levantar e preparar o cafe")
+elif esta_muito_frio == True:
+    print("ficar quietinho na cama")
+elif  esta_muito_quente == True:
+    print("aumentar o volume do ventilador")
+else:
+    print("dormir mais um pouco")
