3bc9ea60a10678095aeacd078401279cb23a7744
lucas gabriel <gabriel.cardosso13@gmail.com>
2025-05-11 23:20:00 -0300

    3.0

---
+++ Python/aula005/app.py
@@ -0,0 +1,8 @@
+#loop
+lista_produtos = ["iphone" , "sansung" , "notebook"]
+for produto in lista_produtos:
+    print(produto)
+
+lista_preco = [1500, 2000 , 5000]
+for preco in lista_preco:
+    print(preco + 50)
